2e2bfce2a298848a7cd7b6fbca0e02300b78f0f5
Hannah Mandell <hfmandell@gmail.com>
2023-04-11 22:18:30 -0700

    close first connection

--- load_tweets.py
+++ load_tweets.py
@@ -91,17 +91,20 @@ def insert_tweet(connection,tweet):
         tweet: a dictionary representing the json tweet object
     '''
 
+    
     # skip tweet if it's already inserted
     sql=sqlalchemy.sql.text('''
     SELECT id_tweets 
     FROM tweets
     WHERE id_tweets = :id_tweets
     ''')
+    
     res = connection.execute(sql,{
         'id_tweets':tweet['id'],
         })
     if res.first() is not None:
         return
+    connection.commit()
 
     # insert tweet within a transaction;
     # this ensures that a tweet does not get "partially" loaded
